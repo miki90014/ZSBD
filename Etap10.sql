37a2d0691e4444fa44627c5984d971120419256f
EC2_BUILD <adam.stypczyc@gmail.com>
2025-01-19 12:32:57 +0100

    Etap10 added

---
+++ Etap10.sql
@@ -0,0 +1,70 @@
+-- Propozycja 1.
+ALTER SYSTEM SET INMEMORY_QUERY=ENABLE;
+
+ALTER TABLE Loans INMEMORY;
+
+EXPLAIN PLAN FOR
+SELECT 
+    b.BookID, 
+    b.Title, 
+    b.PublicationDate, 
+    AVG(rv.Rating) AS AvgRating,  
+    COUNT(DISTINCT l.LoanID) AS TotalLoans,  
+    SUM(CASE 
+            WHEN l.DueDate < SYSDATE AND l.ReturnDate IS NULL THEN 1
+            ELSE 0
+        END) AS TotalDelayedLoans, 
+    COUNT(DISTINCT bc.CopyID) AS TotalCopies,  
+    COUNT(DISTINCT CASE 
+                     WHEN bc.Location LIKE 'Boczna aleja%' THEN bc.CopyID
+                     ELSE NULL
+                   END) AS AlejaLocationCopies,  
+    COUNT(DISTINCT rv.ReviewID) AS TotalReviews,  
+    (SELECT COUNT(*)
+     FROM BookCopies bc2
+     WHERE bc2.BookID = b.BookID
+       AND bc2.IsAvailable = 'Y') AS TotalAvailableCopies  
+FROM 
+    Books b
+JOIN 
+    BookCopies bc ON b.BookID = bc.BookID 
+LEFT JOIN 
+    Reviews rv ON b.BookID = rv.BookID  
+LEFT JOIN 
+    Loans l ON bc.CopyID = l.CopyID 
+LEFT JOIN 
+    Penalties p ON l.ReaderID = p.ReaderID AND p.IsPaid = 'N'  
+WHERE 
+    b.PublicationDate < SYSDATE - INTERVAL '2' YEAR 
+    AND EXISTS (  
+        SELECT 1
+        FROM BookCopies bc2
+        WHERE bc2.BookID = b.BookID
+          AND bc2.Location LIKE 'Boczna aleja%'
+          AND bc2.IsAvailable = 'Y'
+    )
+GROUP BY 
+    b.BookID, b.Title, b.PublicationDate
+HAVING 
+    AVG(rv.Rating) >= 2  
+    AND COUNT(DISTINCT l.LoanID) > 1  
+    AND COUNT(DISTINCT rv.ReviewID) > 1  
+ORDER BY 
+    AvgRating DESC, TotalLoans DESC;
+SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY());
+
+--Eksperyment 1
+ALTER TABLE Loans MOVE NOCOMPRESS;
+
+ALTER TABLE Loans MOVE COMPRESS FOR ALL OPERATIONS;
+
+ALTER TABLE Loans INMEMORY MEMCOMPRESS FOR QUERY LOW;
+
+ALTER TABLE Loans INMEMORY MEMCOMPRESS FOR QUERY HIGH;
+
+ALTER TABLE Loans INMEMORY MEMCOMPRESS FOR CAPACITY LOW;
+
+ALTER TABLE Loans INMEMORY MEMCOMPRESS FOR CAPACITY HIGH;
+
+
+
